ede6d8a79c5e4f24e50bae41404abc59acdb7bd1
luka_120_2019 <lukavukotic811@gmail..com>
2025-01-05 20:22:19 +0100

    Implemented basic CRUD functions for User model, including create, read, update, and delete operations.

---
+++ backend/src/services/userService.mjs
@@ -0,0 +1,33 @@
+import userModel from "../schemas/user.mjs";
+
+export const createUser = async function(newUser){
+    try{
+        const user = await userModel.create(newUser);
+        return user.toJSON();
+    }catch(error){
+        console.error(`Error occured in createUser func: ${error.message}`);
+    }
+}
+
+export const fetchUserByUsername = async function(username){
+    try{
+        const user = await userModel.findOne({"username": username});
+        return user;
+    }catch(error){
+        console.error(`Error occured in fetchUserByUsername func: ${error.message}`);
+    }
+}
+
+export const updateUserByUsername = async function(username, updateBody){
+    //TODO
+}
+
+export const deleteUserByUsername = async function(username){
+    try{
+        const {deletedCount} = await userModel.deleteOne({"username": username});
+        if(deletedCount === 1) return {msg: `Successful deletion of User ${username}`}
+        return {msg: `Unsuccessful deletion of User ${username}`};
+    }catch(error){
+        console.error(`Error occured in deleteUserByUsername func: ${error.message}`);
+    }
+}
