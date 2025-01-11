95eb366e5d33c53b12fe3737c66129f0052a2971
luka_120_2019 <lukavukotic811@gmail..com>
2025-01-11 21:35:06 +0100

    Implementation of basic handlers for endpoints (GET,POST,DELETE). PUT method is yet to be done

---
+++ backend/src/routes/users.mjs
@@ -0,0 +1,55 @@
+import { Router } from "express";
+
+import {createUser,
+        fetchUserByUsername,
+        deleteUserByUsername}
+        from "../services/userService.mjs"
+
+const router = Router();
+
+router.get("/:username", async (req, res) => {
+    try{
+        const username = req.params.username;
+
+        const user = await fetchUserByUsername(username);
+
+        if(!user) return res.status(404).send({msg: `User ${username} not found`});
+
+        return res.status(200).json(user);
+    }catch(error){
+        return res.status(500).json({msg: "Internal server error in fetchUser function"});
+    }   
+});
+
+router.post("/", async (req,res) => {
+    try{
+        const {body} = req;
+        const newUser = await createUser(body);
+
+        res.status(201).json(newUser);
+    }catch(error){
+        if(error.message.startsWith("Validation error: ")){
+            return res.status(400).json({"error": error.message});
+        }
+        return res.status(500).json({"error": "Internal server error"});
+    }
+});
+
+router.put("/:username", async (req, res) => {
+    //TODO
+});
+
+router.delete("/:username", async (req, res) => {
+    try{
+        const username = req.params.username;
+
+        const {deletedCount, acknowledged} = await deleteUserByUsername(username);
+
+        if(deletedCount === 0) return res.status(404).send({msg: `Unsuccessful deletion of User ${username}`});
+            
+        return res.status(200).json({msg: `Successful deletion of User ${username}`});
+
+    }catch(error){
+        res.status(500).json({"error": error.message});
+    }
+});
