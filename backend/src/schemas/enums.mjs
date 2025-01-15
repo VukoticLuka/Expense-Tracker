034ad86dd31a2ef56e8e9924c7c483536195fbbb
luka_120_2019 <lukavukotic811@gmail..com>
2025-01-15 20:09:55 +0100

    Creation of file that will contain all enum objects

---
+++ backend/src/schemas/enums.mjs
@@ -0,0 +1,14 @@
+export const CategoryEnum = Object.freeze({
+    FOOD: 'Food',
+    TRANSPORT: 'Transport',
+    ENTERTAINMENT: 'Entertaiment',
+    HEALTH: 'Health',
+    UTILITIES: 'Utilities',
+    OTHER: 'Other',
+});
+
+export const CurrencyEnum = Object.freeze({
+    EUR: 'EUR',
+    RSD: 'RSD',
+    USD: 'USD',
+});
